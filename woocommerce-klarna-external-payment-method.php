fcdbf79852fb1c664d60d7ee70ddcbff476669a9
Niklas Högefjord <niklas@krokedil.se>
2017-01-10 14:09:43 +0100

    Add customer data to order or create customer

--- woocommerce-klarna-external-payment-method.php
+++ woocommerce-klarna-external-payment-method.php
@@ -48,10 +48,10 @@ function wkemp_klarna_checkout_form_fields( $settings ) {
 add_filter('kco_create_order', 'krokedil_kco_create_order_paypal');
 function krokedil_kco_create_order_paypal( $create ) {
 	$klarna_checkout_settings = get_option( 'woocommerce_klarna_checkout_settings' );
-	$name = $klarna_checkout_settings['epm_paypal_name'];
-	$image_url = $klarna_checkout_settings['epm_paypal_img_url'];
-	$fee = $klarna_checkout_settings['epm_paypal_fee'];
-	$description = $klarna_checkout_settings['epm_paypal_description'];
+	$name   		= ( isset( $klarna_checkout_settings['epm_paypal_name'] ) ) ? $klarna_checkout_settings['epm_paypal_name'] : '';
+	$image_url   	= ( isset( $klarna_checkout_settings['epm_paypal_img_url'] ) ) ? $klarna_checkout_settings['epm_paypal_img_url'] : '';
+	$fee   			= ( isset( $klarna_checkout_settings['epm_paypal_fee'] ) ) ? $klarna_checkout_settings['epm_paypal_fee'] : '';
+	$description   	= ( isset( $klarna_checkout_settings['epm_paypal_description'] ) ) ? $klarna_checkout_settings['epm_paypal_description'] : '';
 	$klarna_external_payment = array(
 		'name' 			=> $name,
 		'redirect_url' 	=> esc_url( add_query_arg( 'kco-external-payment', 'paypal', get_site_url() ) ),
@@ -84,4 +84,154 @@ function kco_redirect_to_paypal() {
 		}
 		
 	}
+}
+
+/**
+ * Add customer data to order if customer exist or if KCO settings say that we should create customer on order completion.
+ *
+ * Also clear KCO sessions if this was a PayPal purchase that was initiated from the KCO iframe.
+ *
+ */
+
+add_action( 'woocommerce_thankyou', 'krokedil_kco_paypal_thankyou' );
+
+function krokedil_kco_paypal_thankyou( $order_id ) {
+	
+	$order = wc_get_order( $order_id );
+	
+	if( $order && 'paypal' == $order->payment_method && WC()->session->get( 'ongoing_klarna_order' ) ) {
+		// Store user id in order so the user can keep track of track it in My account.
+		if ( email_exists( $order->billing_email ) ) {
+			
+			$user = get_user_by( 'email', $order->billing_email );
+
+			$customer_id = $user->ID;
+			update_post_meta( $order->id, '_customer_user', $customer_id );
+		} else {
+			// Get Klarna Checkout settings
+			$checkout_settings = get_option( 'woocommerce_klarna_checkout_settings' );
+			if ( 'yes' === $checkout_settings['create_customer_account'] ) {
+				$password     = '';
+				$new_customer = krokedil_kco_paypal_create_new_customer( $order->billing_email, $order->billing_email, $password );
+				
+				if ( 0 === $new_customer ) { // Creation failed.
+					$order->add_order_note( sprintf( __( 'Customer creation failed.', 'klarna' ) ) );
+					$customer_id = 0;
+				} else { // Creation succeeded.
+					$order->add_order_note( sprintf( __( 'New customer created (user ID %s).', 'klarna' ), $new_customer ) );
+
+					// Add customer name.
+					update_user_meta( $new_customer, 'first_name', $order->billing_first_name );
+					update_user_meta( $new_customer, 'last_name', $order->billing_last_name );
+
+					// Add customer billing address.
+					update_user_meta( $new_customer, 'billing_first_name', $order->billing_first_name );
+					update_user_meta( $new_customer, 'billing_last_name', $order->billing_last_name );
+					update_user_meta( $new_customer, 'billing_address_1', $order->billing_address_1 );
+					update_user_meta( $new_customer, 'billing_address_2', $order->billing_address_2 );
+					update_user_meta( $new_customer, 'billing_postcode', $order->billing_postcode );
+					update_user_meta( $new_customer, 'billing_city', $order->billing_city );
+					update_user_meta( $new_customer, 'billing_country', $order->billing_country );
+					update_user_meta( $new_customer, 'billing_email', $order->billing_email );
+					update_user_meta( $new_customer, 'billing_phone', $order->billing_phone );
+
+					// Add customer shipping address.
+					if ( $order->shipping_last_name ) { // Input var okay.
+						update_user_meta( $new_customer, 'shipping_first_name', $order->shipping_first_name );
+						update_user_meta( $new_customer, 'shipping_last_name', $order->shipping_last_name );
+						update_user_meta( $new_customer, 'shipping_address_1', $order->shipping_address_1 );
+						update_user_meta( $new_customer, 'shipping_address_2', $order->shipping_address_2 );
+						update_user_meta( $new_customer, 'shipping_postcode', $order->shipping_postcode );
+						update_user_meta( $new_customer, 'shipping_city', $order->shipping_city );
+						update_user_meta( $new_customer, 'shipping_country', $order->shipping_country );
+					}
+
+					$customer_id = $new_customer;
+					update_post_meta( $order->id, '_customer_user', $customer_id );
+					// Test set user.
+					wp_set_current_user( $customer_id );
+					wc_set_customer_auth_cookie( $customer_id );
+					// As we are now logged in, checkout will need to refresh to show logged in data
+					WC()->session->set( 'reload_checkout', true );
+				}
+
+				
+			}
+		}
+		
+		// Clear KCO sessions
+		WC()->session->__unset( 'klarna_checkout' );
+		WC()->session->__unset( 'klarna_checkout_country' );
+		WC()->session->__unset( 'ongoing_klarna_order' );
+		WC()->session->__unset( 'klarna_order_note' );
+	}
+}
+
+/**
+ * Create a new customer
+ *
+ * @param string $email    Customer email.
+ * @param string $username Customer username.
+ * @param string $password Customer password.
+ *
+ * @return mixed WP_error on failure, Int (user ID) on success
+ */
+function krokedil_kco_paypal_create_new_customer( $email, $username = '', $password = '' ) {
+	// Check the e-mail address.
+	if ( empty( $email ) || ! is_email( $email ) ) {
+		return new WP_Error( 'registration-error', __( 'Please provide a valid email address.', 'woocommerce' ) );
+	}
+
+	if ( email_exists( $email ) ) {
+		return new WP_Error( 'registration-error', __( 'An account is already registered with your email address. Please login.', 'woocommerce' ) );
+	}
+
+	// Handle username creation.
+	$username = sanitize_user( current( explode( '@', $email ) ) );
+
+	// Ensure username is unique.
+	$append     = 1;
+	$o_username = $username;
+	while ( username_exists( $username ) ) {
+		$username = $o_username . $append;
+		$append ++;
+	}
+
+	// Handle password creation.
+	$password           = wp_generate_password();
+	$password_generated = true;
+
+	// WP Validation.
+	$validation_errors = new WP_Error();
+	do_action( 'woocommerce_register_post', $username, $email, $validation_errors );
+	$validation_errors = apply_filters( 'woocommerce_registration_errors', $validation_errors, $username, $email );
+
+	if ( $validation_errors->get_error_code() ) {
+
+		return 0;
+	}
+
+	$new_customer_data = apply_filters( 'woocommerce_new_customer_data', array(
+		'user_login' => $username,
+		'user_pass'  => $password,
+		'user_email' => $email,
+		'role'       => 'customer',
+	) );
+
+	$customer_id = wp_insert_user( $new_customer_data );
+
+	if ( is_wp_error( $customer_id ) ) {
+		$validation_errors->add( 'registration-error', '<strong>' . __( 'ERROR', 'woocommerce' ) . '</strong>: ' . __( 'Couldn&#8217;t register you&hellip; please contact us if you continue to have problems.', 'woocommerce' ) );
+
+		return 0;
+	}
+
+	// Send New account creation email to customer?
+	$checkout_settings = get_option( 'woocommerce_klarna_checkout_settings' );
+
+	if ( 'yes' === $checkout_settings['send_new_account_email'] ) {
+		do_action( 'woocommerce_created_customer', $customer_id, $new_customer_data, $password_generated );
+	}
+
+	return $customer_id;
 }
