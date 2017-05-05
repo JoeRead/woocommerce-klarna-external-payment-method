44df859a3a53baae358910d1e8dd9b2d2210d63c
Slobodan Manic <slobodan.manic@gmail.com>
2017-05-05 11:15:58 +0200

    Fixes #1

--- woocommerce-klarna-external-payment-method.php
+++ woocommerce-klarna-external-payment-method.php
@@ -9,81 +9,83 @@ Author URI: http://krokedil.com
 */
 
 /**
- * Extens KCO settings with External Payment Method - PayPal settings.
- *
+ * Extends KCO settings with External Payment Method - PayPal settings.
  */
 add_filter( 'klarna_checkout_form_fields', 'wkemp_klarna_checkout_form_fields' );
-
 function wkemp_klarna_checkout_form_fields( $settings ) {
 	$settings['epm_paypal_settings_title'] = array(
 		'title' => __( 'External Payment Method - PayPal', 'woocommerce-gateway-klarna' ),
 		'type'  => 'title',
 	);
-	$settings['epm_paypal_name'] = array(
+	$settings['epm_paypal_name']           = array(
 		'title'       => __( 'Name', 'woocommerce-gateway-klarna' ),
 		'type'        => 'text',
 		'description' => __( 'Title for PayPal payment method. This controls the title which the user sees in the checkout form.', 'woocommerce-gateway-klarna' ),
 		'default'     => __( 'PayPal', 'woocommerce-gateway-klarna' )
 	);
-	$settings['epm_paypal_description'] = array(
+	$settings['epm_paypal_description']    = array(
 		'title'       => __( 'Description', 'woocommerce-gateway-klarna' ),
 		'type'        => 'textarea',
 		'description' => __( 'Description for PayPal payment method. This controls the description which the user sees in the checkout form.', 'woocommerce-gateway-klarna' ),
 		'default'     => ''
 	);
-	$settings['epm_paypal_img_url'] = array(
+	$settings['epm_paypal_img_url']        = array(
 		'title'       => __( 'Image url', 'woocommerce-gateway-klarna' ),
 		'type'        => 'text',
 		'description' => __( 'The url to the PayPal payment Icon.', 'woocommerce-gateway-klarna' ),
 		'default'     => 'https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-100px.png'
 	);
-	$settings['epm_paypal_fee'] = array(
+	$settings['epm_paypal_fee']            = array(
 		'title'       => __( 'Fee', 'woocommerce-gateway-klarna' ),
 		'type'        => 'text',
 		'description' => __( 'Fee added to the order.', 'woocommerce-gateway-klarna' ),
 		'default'     => ''
 	);
-	
+
 	return $settings;
 }
 
 /**
  * Add PayPal as Payment Method to the KCO iframe.
- *
  */
-add_filter('kco_create_order', 'krokedil_kco_create_order_paypal');
+add_filter( 'kco_create_order', 'krokedil_kco_create_order_paypal' );
 function krokedil_kco_create_order_paypal( $create ) {
 	$klarna_checkout_settings = get_option( 'woocommerce_klarna_checkout_settings' );
-	$name   		= ( isset( $klarna_checkout_settings['epm_paypal_name'] ) ) ? $klarna_checkout_settings['epm_paypal_name'] : '';
-	$image_url   	= ( isset( $klarna_checkout_settings['epm_paypal_img_url'] ) ) ? $klarna_checkout_settings['epm_paypal_img_url'] : '';
-	$fee   			= ( isset( $klarna_checkout_settings['epm_paypal_fee'] ) ) ? $klarna_checkout_settings['epm_paypal_fee'] : '';
-	$description   	= ( isset( $klarna_checkout_settings['epm_paypal_description'] ) ) ? $klarna_checkout_settings['epm_paypal_description'] : '';
-	$klarna_external_payment = array(
-		'name' 			=> $name,
-		'redirect_url' 	=> esc_url( add_query_arg( 'kco-external-payment', 'paypal', get_site_url() ) ),
-		'image_url' 	=> $image_url,
-		'fee' 			=> $fee,
-		'description' 	=> $description,
+	$name                     = ( isset( $klarna_checkout_settings['epm_paypal_name'] ) ) ? $klarna_checkout_settings['epm_paypal_name'] : '';
+	$image_url                = ( isset( $klarna_checkout_settings['epm_paypal_img_url'] ) ) ? $klarna_checkout_settings['epm_paypal_img_url'] : '';
+	$fee                      = ( isset( $klarna_checkout_settings['epm_paypal_fee'] ) ) ? $klarna_checkout_settings['epm_paypal_fee'] : '';
+	$description              = ( isset( $klarna_checkout_settings['epm_paypal_description'] ) ) ? $klarna_checkout_settings['epm_paypal_description'] : '';
+	$klarna_external_payment  = array(
+		'name'         => $name,
+		'image_url'    => $image_url,
+		'fee'          => $fee,
+		'description'  => $description,
 	);
-	$klarna_external_payment = array( $klarna_external_payment );
-		
+
+	if ( array_key_exists( 'merchant_urls', $create ) ) { // V3
+		$klarna_external_payment['redirect_url'] = esc_url( add_query_arg( 'kco-external-payment', 'paypal', get_site_url() ) );
+	} else { // V2
+		$klarna_external_payment['redirect_uri'] = esc_url( add_query_arg( 'kco-external-payment', 'paypal', get_site_url() ) );
+	}
+
+	$klarna_external_payment  = array( $klarna_external_payment );
+
 	$create['external_payment_methods'] = $klarna_external_payment;
-	
+
 	return $create;
 }
 
 /**
  * Redirect to PayPal when the Proceed to PayPal button in KCO iframe is clicked.
- *
  */
-add_action('init', 'kco_redirect_to_paypal' );
+add_action( 'init', 'kco_redirect_to_paypal' );
 function kco_redirect_to_paypal() {
-	if( isset ( $_GET['kco-external-payment'] ) && 'paypal' == $_GET['kco-external-payment'] ) {
+	if ( isset ( $_GET['kco-external-payment'] ) && 'paypal' == $_GET['kco-external-payment'] ) {
 		$paypal_gateway = new WC_Gateway_Paypal();
-  		include_once( ABSPATH . 'wp-content/plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php' );
-		$order          = wc_get_order( WC()->session->get( 'ongoing_klarna_order' ) );
-		if( $order ) {
-			$paypal_request = new WC_Gateway_Paypal_Request( $paypal_gateway );
+		include_once( ABSPATH . 'wp-content/plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php' );
+		$order = wc_get_order( WC()->session->get( 'ongoing_klarna_order' ) );
+		if ( $order ) {
+			$paypal_request     = new WC_Gateway_Paypal_Request( $paypal_gateway );
 			$available_gateways = WC()->payment_gateways->payment_gateways();
 			$payment_method     = $available_gateways['paypal'];
 			$order->set_payment_method( $payment_method );
@@ -91,7 +93,7 @@ function kco_redirect_to_paypal() {
 			wp_redirect( $paypal_request->get_request_url( $order, $paypal_gateway->testmode ) );
 			exit;
 		}
-		
+
 	}
 }
 
@@ -99,19 +101,15 @@ function kco_redirect_to_paypal() {
  * Add customer data to order (in thank you page) if customer exist or if KCO settings say that we should create customer on order completion.
  *
  * Also clear KCO sessions if this was a PayPal purchase that was initiated from the KCO iframe.
- *
  */
-
 add_action( 'woocommerce_thankyou', 'krokedil_kco_paypal_thankyou' );
-
 function krokedil_kco_paypal_thankyou( $order_id ) {
-	
 	$order = wc_get_order( $order_id );
-	
-	if( $order && 'paypal' == $order->payment_method && WC()->session->get( 'ongoing_klarna_order' ) ) {
+
+	if ( $order && 'paypal' == $order->payment_method && WC()->session->get( 'ongoing_klarna_order' ) ) {
 		// Store user id in order so the user can keep track of track it in My account.
 		if ( email_exists( $order->billing_email ) ) {
-			
+
 			$user = get_user_by( 'email', $order->billing_email );
 
 			$customer_id = $user->ID;
@@ -122,7 +120,7 @@ function krokedil_kco_paypal_thankyou( $order_id ) {
 			if ( 'yes' === $checkout_settings['create_customer_account'] ) {
 				$password     = '';
 				$new_customer = krokedil_kco_paypal_create_new_customer( $order->billing_email, $order->billing_email, $password );
-				
+
 				if ( 0 === $new_customer ) { // Creation failed.
 					$order->add_order_note( sprintf( __( 'Customer creation failed.', 'klarna' ) ) );
 					$customer_id = 0;
@@ -164,10 +162,10 @@ function krokedil_kco_paypal_thankyou( $order_id ) {
 					WC()->session->set( 'reload_checkout', true );
 				}
 
-				
+
 			}
 		}
-		
+
 		// Clear KCO sessions
 		WC()->session->__unset( 'klarna_checkout' );
 		WC()->session->__unset( 'klarna_checkout_country' );
@@ -179,7 +177,7 @@ function krokedil_kco_paypal_thankyou( $order_id ) {
 /**
  * Create a new customer
  *
- * @param string $email    Customer email.
+ * @param string $email Customer email.
  * @param string $username Customer username.
  * @param string $password Customer password.
  *
